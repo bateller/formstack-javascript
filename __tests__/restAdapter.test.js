32a296f99d6f7d052c59a2896e394c938afa274b
Brian Teller <brian.teller@formstack.com>
2024-01-08 13:28:52 -0500

    Add init of tests

---
+++ __tests__/restAdapter.test.js
@@ -0,0 +1,16 @@
+const { FormsClient } = require('../restAdapter');
+
+describe('FormsClient', () => {
+    let client;
+
+    beforeAll(() => {
+        client = new FormsClient('hostname', 'token');
+        // Mock axios or other dependencies needed
+    });
+
+    test('getForm should return form data', async () => {
+	// TODO: Write test
+    });
+
+    // TODO: Write more tests...
+});
